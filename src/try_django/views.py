06c2b91406518308a0f85c2ccace330f1ba6f838
kevinjolley91 <jolley427@gmail.com>
2023-11-20 16:35:12 -0500

    learning about templates

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -1,12 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template.loader import get_template
 
 
 def home_page(request):
     my_title = "Hello there..."
-    # doc = "<h1>{title}</h1>".format(title=title)
-    # django_rendered_doc = "<h1>{{title}}</h1>".format(title=title)
-    return render(request, "hello_world.html", {"title": my_title})
+    context = {"title": my_title}
+    return render(request, "home.html", context)
 
 
 def about_page(request):
@@ -15,3 +15,11 @@ def about_page(request):
 
 def contact_page(request):
     return render(request, "hello_world.html", {"title": "Contact Us"})
+
+
+def example_page(request):
+    context = {"title": "Example"}
+    template_name = "hello_world.html"
+    template_obj = get_template(template_name)
+    rendered_item = template_obj.render(context)
+    return HttpResponse(rendered_item)
