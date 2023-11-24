16a91c29388111491d706134b3cfcbec725a28a2
kevinjolley91 <jolley427@gmail.com>
2023-11-24 15:59:10 -0500

    working with slugs

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
 
-def blog_post_detail_page(request):
-    obj = BlogPost.objects.get(id=1)
+def blog_post_detail_page(request, slug):
+    # queryset = BlogPost.objects.filter(slug=slug)
+    # if queryset.count() == 0:
+    #     raise Http404
+    # obj = queryset.first()
+    obj = get_object_or_404(BlogPost, slug=slug)
     template_name = "blog_post_detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
--- src/try_django/urls.py
+++ src/try_django/urls.py
@@ -28,8 +28,11 @@ from .views import (
 
 urlpatterns = [
     path('', home_page),
-    path('blog/', blog_post_detail_page),
-    # re_path(r'^pages?/$', about_page),
+    path('blog/<str:slug>/', blog_post_detail_page),
+    re_path(r'^blog/(?P<post_id>\d+)/$', blog_post_detail_page),
+    path('page/', about_page),
+    path('pages/', about_page),
+    re_path(r'^pages?/$', about_page),
     re_path(r'^about/$', about_page),
     path('example/', example_page),
     path('contact/', contact_page),
