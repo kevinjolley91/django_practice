31d042460e8446ec2b5a290db7e20b1efb517295
kevinjolley91 <jolley427@gmail.com>
2023-12-04 15:14:49 -0500

    template updated for consistent design

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 from django.template.loader import get_template
 
 from .forms import ContactForm
+from blog.models import BlogPost
 
 
 def home_page(request):
     my_title = "Hello there..."
-    context = {"title": my_title, "my_list": [1, 2, 3, 4, 5]}
+    qs = BlogPost.objects.all()[:5]
+    context = {"title": "Welcome to Try Django", 'blog_list': qs}
     return render(request, "home.html", context)
 
 
